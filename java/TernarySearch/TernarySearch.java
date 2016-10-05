975799a0cd8d2aefa49de492e96043eaf999d617
JB Kim <jb@jdlab.org>
2016-10-05 20:46:19 +0900

    init TernarySearch

---
+++ java/TernarySearch/TernarySearch.java
@@ -0,0 +1,20 @@
+class TernarySearch {
+	static int ternarySearch(char[] haystack, char needle){
+
+
+	}
+
+	public static void main(String[][] args){
+		if (args.length() < 2){
+			System.out.println("Usage: java TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND");
+			System.out.println("ex) java TernarySearch abklmxyz x");
+			System.out.println("\t*the STRING should be sorted.");
+			return;
+		}
+		for (int i = 0; i < args[0].length(); i++){
+		}
+		
+
+	
+	}
+}
