bca373aef5c0578f29ecbaa3e315601c737b523d
JB Kim <jb@jdlab.org>
2016-10-07 16:24:33 +0900

    backup: calculateDistribution method

--- java/TernarySearch/TernarySearch.java
+++ java/TernarySearch/TernarySearch.java
@@ -1,3 +1,6 @@
+import java.util.Map;
+import java.util.HashMap;
+
 class TernarySearch {
 	public static final int DEFAULT_TEST_REPS = 10000;
 
@@ -28,20 +31,62 @@ class TernarySearch {
 		return index;
 	}
 
-	static int fastTernarySearch(char[] haystack, char needle, char[][] distribution){
+	static int fastTernarySearch(char[] haystack, char needle, Map<Character, Double> distribution){
 		int index = -1;
+
 		return index;
+	}
+
+	static Map<Character, Double> calculateDistribution(char[][] data_history){
+		Map<Character, Double> distributionMap = new HashMap<Character, Double>();
+		Map<Character, Integer> countMap = new HashMap<Character, Integer>();
+
+		int len;
+		char ch;
+		Double sum;
+		Double position;
+		
+		for (char[] record : data_history){
+			len = record.length;
+			for (int i = 0; i < len; i++){
+				ch = record[i];
+				position = i * 1.0 / len;
+				sum = distributionMap.get(ch);
+				if (sum == null){
+					distributionMap.put(ch, position);
+					countMap.put(ch, 1);
+				} else {
+					distributionMap.put(ch, sum + position);
+					countMap.put(ch, countMap.get(ch) + 1);
+				}
+			}
+		}
+
+		for (Character key : distributionMap.keySet()){
+			distributionMap.put(key, distributionMap.get(key) / countMap.get(key));
+		}
+
+		return distributionMap;
+	}
+
+	static void compare(char[] haystack, char needle, Map<Character, Double> distribution){
+		
 	}
 
 	public static void main(String[] args){
 		if (args.length < 2){
-			System.out.println("Usage1: java TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND");
+			System.out.println("Usage1: naive ternary search");
+			System.out.println("\njava TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND");
 			System.out.println("\t* the STRING should be sorted.");
 			System.out.println("ex) java TernarySearch abklmxyz x");
-			System.out.println("\nUsage2: java TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND DATA_DISTRIBUTION [TEST_REPS]");
+			System.out.println("\nUsage2: fast ternary search with given data distribution");
+			System.out.println("\njava TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND DATA_DISTRIBUTION");
 			System.out.println("\t* the DATA_DISTRIBUTION is comma-separated string which contains previous STRINGs.");
-			System.out.format("\t* if TEST_REPS is given, comparison(naive search vs. with distribution) will be done with TEST_REPS times, otherwise default value (%d) will be used.\n", DEFAULT_TEST_REPS);
-			System.out.println("ex) java TernarySearch abklmxyz x abcdefg,vwxyz,abcxyz\n");
+			System.out.println("ex) java TernarySearch abklmxyz x abcdefg,vwxyz,abcxyz");
+			System.out.println("\nUsage3: comparison (naive ternary search vs. fast ternary search with given data distribution)");
+			System.out.println("\njava TernarySearch STRING_FOR_DATA_ARR CHAR_TO_FIND DATA_DISTRIBUTION TEST_REPS");
+			System.out.println("\t* repeat both ternary searches for TEST_REPS times, and compare elapsed time");
+			System.out.println("ex) java TernarySearch abklmxyz x abcdefg,vwxyz,abcxyz 10000");
 			return;
 		}
 
@@ -54,6 +99,18 @@ class TernarySearch {
 			char[][] dist_arr = new char[dist_str.length][];
 			for (int i = 0; i < dist_str.length; i++){
 				dist_arr[i] = dist_str[i].toCharArray();
+			}
+			Map<Character, Double> distribution = calculateDistribution(dist_arr);
+
+			if (args.length > 3) {
+
+			} else {
+				index = fastTernarySearch(array, ch, distribution);
+				if (index < 0){
+					System.out.format("%c not found", ch);
+				} else {
+					System.out.format("%c is at %d\n", ch, index + 1);
+				}
 			}
 		} else {
 			index = simpleTernarySearch(array, ch);
